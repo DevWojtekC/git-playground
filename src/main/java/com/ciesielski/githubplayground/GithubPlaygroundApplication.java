0da261caacb5de3c6074e356d931950ea8329dca
wciesielski <wojciech.ciesielskI@grapeup.com>
2020-08-27 09:54:40 +0200

    Initial commit

---
+++ src/main/java/com/ciesielski/githubplayground/GithubPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.ciesielski.githubplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubPlaygroundApplication.class, args);
+	}
+
+}
